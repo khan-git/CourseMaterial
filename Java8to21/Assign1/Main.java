c0eeb0aae52040299fa08a39cece3e4cb3650a05
khan-git <khan-git@users.noreply.github.com>
2024-01-13 16:26:08 +0000

    WIP

--- Java8to21/Assign1/Main.java
+++ Java8to21/Assign1/Main.java
@@ -11,12 +11,17 @@ public class Main {
     }
 
     private static List<Person> getPeople() {
+        
         List<Person> result = new ArrayList<>();
         result.add(new Person("Mike", 33, 1.8));
         result.add(new Person("Mary", 25, 1.4));
         result.add(new Person("Alan", 34, 1.7));
         result.add(new Person("Zoe", 30, 1.5));
         return result;
+    }
+
+    void consumer(List<Person> people) {
+
     }
 
 }
